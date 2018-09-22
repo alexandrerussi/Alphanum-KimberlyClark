f9a8758ca416cb555741310df1e698d48a4850c8
Alexandre Russi <junior.russi2011@gmail.com>
2018-09-21 23:35:59 -0300

    Estilizando tela inicial

--- AlphanumKC/app/src/main/java/com/alphanum/plenitud/alphanumkc/HomeActivity.java
+++ AlphanumKC/app/src/main/java/com/alphanum/plenitud/alphanumkc/HomeActivity.java
@@ -1,7 +1,13 @@
 package com.alphanum.plenitud.alphanumkc;
 
+import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,14 +17,29 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
+
+    //private Button btnHomeQRCode;
+
+    private static final int BARCODE_RECO_REQ_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,21 +60,36 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MapFragment()).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_button, new ButtonFragment()).commit();
+        }
+
+        //Importando botões
+        //btnHomeQRCode = (Button) findViewById(R.id.btnHomeQRCode);
+
+        //Setando click botões
+        /*btnHomeQRCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                barcodeReco();
+            }
+        });*/
+
     }
 
     @Override
     public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else {
+        } else {
             super.onBackPressed();
         }
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.nav_conta:
                 Intent intent = new Intent(HomeActivity.this, PerfilActivity.class);
                 startActivity(intent);
@@ -77,27 +113,67 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     }
 
 
-    /*@SuppressWarnings("StatementWithEmptyBody")
+    //----------------------------------------------------
+    //QR CODE CODIGO INICIO
+
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        if (id == R.id.nav_conta) {
-            Intent intent = new Intent(HomeActivity.this, PerfilActivity.class);
-            startActivity(intent);
-        }
-        else if (id == R.id.nav_carteira) {
-            Toast.makeText(this, "a", Toast.LENGTH_SHORT).show();
-        }
-        else if (id == R.id.nav_loggout) {
-
+        if (requestCode == BARCODE_RECO_REQ_CODE) {
+            if (resultCode == RESULT_OK) {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                barcodeRecognition(photo);
+            }
         }
 
+    }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }*/
+    private void barcodeRecognition(Bitmap photo) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(photo);
+        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
+                .getVisionBarcodeDetector();
+        Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
+                        for (FirebaseVisionBarcode barcode : barcodes) {
+                            Rect bounds = barcode.getBoundingBox();
+                            Point[] corners = barcode.getCornerPoints();
+
+                            String rawValue = barcode.getRawValue();
+
+                            int valueType = barcode.getValueType();
+                            //edtName.setText(rawValue);
+                            Toast.makeText(HomeActivity.this, rawValue, Toast.LENGTH_LONG).show();
+
+                            if (rawValue != null) {
+                                Toast.makeText(HomeActivity.this, "QR recebido", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(HomeActivity.this, "QR vazio", Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(HomeActivity.this, "Erro", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    public void barcodeReco(View view) {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, BARCODE_RECO_REQ_CODE);
+
+    }
+
+    //QR CODE CODIGO FIM
+    //----------------------------------------------------
 
 }
